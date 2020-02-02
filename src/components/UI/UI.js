54439541926baaccf616e3968e5596cabbd7c67b
Artur Abdrakhmanov <aabdrakhmanov@stack-kazan.net>
2020-02-02 10:57:40 +0300

    add button

--- src/components/UI/UI.js
+++ src/components/UI/UI.js
@@ -4,6 +4,7 @@ import Select from '../common/inputs/Select/Select'
 import FormField from '../common/inputs/FormField/FormField'
 import TextArea from '../common/inputs/TextArea/TextArea'
 import CheckBox from '../common/inputs/CheckBox/CheckBox'
+import Button from '../common/Button/Button'
 
 const UI = (props) => {
     return (
@@ -20,6 +21,21 @@ const UI = (props) => {
             <TextArea />
             <br/>
             <CheckBox label='Interested' />
+            <br/>
+            <Button label='active' active/>
+            <br/>
+            <Button label='Edit'/>
+            <br/>
+            <form onSubmit={(e) => {
+                e.preventDefault()
+                console.log(e.target.ss.value)
+            }
+                }>
+                <TextInput name='ss' value='Артур' />
+                <br/>
+                <br/>
+                <Button label='Edit' primary/>
+            </form>
         </div>
     )
 }
